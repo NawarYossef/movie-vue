1428e6ebc0ad0500ba40a9f42151dfbdf215c2a8
Nawar Yossef <nawaryossef2@gmail.com>
2018-03-22 13:44:43 -0400

    add profile component

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import AppNav from '../components/AppNav';
+import PropTypes from 'prop-types';
+import UserImage from "../components/UserImage";
+import UserInfo from "../components/UserInfo";
+import AppNav from "../components/AppNav";
+import EditButton from "../components/EditButton";
+import "./styles/profile.css";
 
-const Profile = props => {
-  return (
-    <section className="settings">
-      <AppNav />
-      <h2>Settings</h2>
-    </section>
-  );
-};
+class Profile extends Component {
+  render() {
+    return (
+      <section className="profile">
+        <AppNav />
+        <UserImage />
+        <UserInfo />
+        <EditButton />
+      </section>
+    );
+  }
+}
 
 export default connect()(Profile);
