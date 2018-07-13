beee9634699f742987e08218e19475d24673e66d
nawar yossef <nawaryossef2@gmail.com>
2018-07-13 12:45:24 -0500

    change variable name

--- src/constants.js
+++ src/constants.js
@@ -3,17 +3,17 @@ const CURRENT_DATE = new Date(new Date().toString().split('GMT')[0]+' UTC').toIS
 export const MOVIES_DATA = {
   nowPlaying: {
     title: 'Now Playing',
-    apiUrl: `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=`,
+    apiUrl: `https://api.themoviedb.org/3/movie/now_playing?api_key=${REACT_APP_API_KEY}&language=en-US&page=`,
     route: '/movies/now-playing'
   },
   comingSoon: {
     title: 'Coming Soon',
-    apiUrl: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1&language=en-US&primary_release_date.gte=${CURRENT_DATE  }&page=`,
+    apiUrl: `https://api.themoviedb.org/3/discover/movie?api_key=${REACT_APP_API_KEY}&language=en-US&page=1&language=en-US&primary_release_date.gte=${CURRENT_DATE  }&page=`,
     route: '/movies/coming-soon'
   },
   popular: {
     title: 'Popular',
-    apiUrl: `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=`,
+    apiUrl: `https://api.themoviedb.org/3/movie/popular?api_key=${REACT_APP_API_KEY}&language=en-US&page=`,
     route: '/movies/popular'
   },
 }
