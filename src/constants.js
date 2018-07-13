3649fed968af07d4074474dba15a51024a8b398c
nawar yossef <nawaryossef2@gmail.com>
2018-07-13 12:48:25 -0500

    add newline

--- src/constants.js
+++ src/constants.js
@@ -16,6 +16,7 @@ export const MOVIES_DATA = {
     apiUrl: `https://api.themoviedb.org/3/movie/popular?api_key=${REACT_APP_API_KEY}&language=en-US&page=`,
     route: '/movies/popular'
   },
+  
 }
 
 export const YOUTUBE_BASE_URL = 'https://www.youtube.com/embed/';
