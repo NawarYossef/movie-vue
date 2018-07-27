f863bab80ce3a070f7d1a0444e58c709afa3b699
nawar yossef <nawaryossef2@gmail.com>
2018-07-27 15:40:27 -0500

    use herku server as base url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 module.exports = {
     API_BASE_URL:
-    //   process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080'
-      'http://localhost:8080'
+      process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080'
   };
